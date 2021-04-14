bff16754c1508e7a083f52f54421bab0de142024
Joe Cianflone <joe@cianflone.co>
2021-04-14 17:26:33 -0400

    adds zshrc file and updates links

--- _setup/links.sh
+++ _setup/links.sh
@@ -6,5 +6,6 @@ ln -sfv "$DOTFILES_DIR/dots/.gemrc" ~
 ln -sfv "$DOTFILES_DIR/dots/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/dots/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/dots/.zprofile" ~
+ln -sfv "$DOTFILES_DIR/dots/.zshrc" ~
 
 
